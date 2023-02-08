f702b733018a50e916d80d35e4f1f4c4c46293bc
hernnnnnn <1211202010@student.mmu.edu.my>
2023-02-08 22:51:56 +0800

    hg

--- admin_session.php
+++ admin_session.php
@@ -4,7 +4,7 @@ $conn = Connect();
 
 session_start();
 
-$user_check=$_SESSION['login_user1'];
+$user_check=$_SESSION['login_user1'];hh
 
 $query = "SELECT username FROM `admin` WHERE username = '$user_check'";
 $ses_sql = mysqli_query($conn, $query);
