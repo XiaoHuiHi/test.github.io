96886026f2c78589fd60bb415ab07be9381f6992
hernnnnnn <1211202010@student.mmu.edu.my>
2023-02-08 19:43:52 +0800

    update

--- adminloginpage.php
+++ adminloginpage.php
@@ -13,7 +13,7 @@ header("location: loading_dashboard.php");
         <title> Admin Login | 1 Coin Sandwich' </title>
     </head>
 
-    <link rel="stylesheet" href ="adminloginpage.css">
+    <link rel="stylesheet" href ="css/adminloginpage.css">
     <link rel="stylesheet" href="https://maxst.icons8.com/vue-static/landings/line-awesome/line-awesome/1.3.0/css/line-awesome.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
