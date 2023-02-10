997d827b87d1df70daa80a761e038eec317c2fd7
hernnnnnn <1211202010@student.mmu.edu.my>
2023-02-11 04:07:44 +0800

    adaa

--- wishlist.php
+++ wishlist.php
@@ -8,7 +8,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <title>Wishlist</title>
+    <title>Wishlist | 1 Coin Sandwich</title>
     <!-- <link rel="stylesheet" href="./Wishlist_Page.css"> -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
